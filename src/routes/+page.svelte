4d8d8b46527db4480a55be080a1c56970f2b8145
Jaarabytes <xavierandole78@gmail.com>
2024-07-04 13:37:52 +0300

    style: improve responsiveness
    Improve responsiveness for the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,16 +9,21 @@
     const builders = [{ name: "Jaarabytes", x: 'https://x.com/Jaarabytes' }]
 </script>
 
+<svelte:head>
+  <title>Calm </title>
+</svelte:head>
+
 <!-- This will be continued later, thank you -->
 <div class='px-3 sm:px-[20%]'>
-    <h1 class="text-[50px] font-extrabold my-3">Compose on calm.</h1>
-    <p class="text-xl font-bold">Compose on calm helps enjoyers & idealists compose, create and listen to classical music.</p>
+    <h1 class="text-3xl sm:text-[50px] font-extrabold my-5">Compose on calm.</h1>
+    <p class="text-lg sm:text-xl font-bold">We help enjoyers & idealists compose, create and listen to classical music.</p>
     <p class='my-5'>We are integrating Machine Learning models & python libraries for audio surgery and yes, you <b>CAN</b> compose with AI</p>
     <p class='my-5'>We will also <b>teach</b> you how to compose basic music patterns.</p>
     <p>Builders: </p>
     {#each builders as builder, i}
         <li><a href={builder.x}>{builder.name}</a></li>
     {/each}
-    <p class='my-5'>Contact us through X (formerly twitter) Dm's. thank you!</p>
-    <p>Listen to <a href="https://open.spotify.com/playlist/2qXjPCmMuGRlVDRRFFetAs?si=c81729c302b54fed"><b>this!</b></a></p>
+    <p class='my-5'>Contact us through X (formerly twitter) Dm's. </p>
+    <p>We also accept <a href="/donate" class="font-bold">donations</a></p> 
+    <p class='my-3'>Listening to <a href="https://open.spotify.com/playlist/2qXjPCmMuGRlVDRRFFetAs?si=c81729c302b54fed"><b>this!</b></a></p>
 </div>
