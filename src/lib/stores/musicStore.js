2bf8a512e1a5c747faf012828b9ecfb2b84b91f2
Jaarabytes <xavierandole78@gmail.com>
2024-07-06 14:22:33 +0300

    to update the set value, to set to random track
    Screw me and screw svelte

---
+++ src/lib/stores/musicStore.js
@@ -0,0 +1,25 @@
+import { writable } from "svelte/store";
+
+// add more tracks  
+const tracks = [
+  {name: "Requiem Aranea", link: "https://p.scdn.co/mp3-preview/0706a174add19c63c3b0eac4a7f066afe0f04c73?cid=cfe923b2d660439caf2b557b21f31221"},
+  {name: "Requiem Lacrimosa, K 626 IIiI", link: "https://p.scdn.co/mp3-preview/7163d81fa06e3939bf3572d186c22d712f806318?cid=cfe923b2d660439caf2b557b21f31221"},
+]
+
+// default track value
+export let currentTrack = writable({
+  name: "Requiem Aranea", 
+  link: "https://p.scdn.co/mp3-preview/0706a174add19c63c3b0eac4a7f066afe0f04c73?cid=cfe923b2d660439caf2b557b21f31221"
+});
+
+// func that randomly picks any track;
+function pickAndRoll() {
+  const randomIndex = Math.floor(Math.random() * tracks.length);
+  const currentTrack = {
+    name: tracks[randomIndex].name,
+    link: tracks[randomIndex].link
+  }
+  return currentTrack;
+}
+
+// find a way to update state with this above
