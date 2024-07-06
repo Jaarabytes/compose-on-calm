09e2004a65148575ead1914b9d3af7f6978cf8e2
Jaarabytes <xavierandole78@gmail.com>
2024-07-06 13:39:48 +0300

    feat: added http dog
    add http dog to handle 404 and 500 server errors

---
+++ src/routes/+error.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { page } from '$app/stores'
+  import { onMount } from 'svelte'
+  // Please use http status dogs
+  import '../App.css'
+
+  // haha, already using it 
+
+  let dogImage = '';
+  
+  // okay, this is exactly useEffect
+  onMount(() => {
+    if ( $page.status === 404 || $page.status === 500 ) {
+      // only for 404, 500 and 401 maybe
+      dogImage = `https://http.dog./${$page.status}.jpg`
+    }
+  })
+</script>
+
+<svelte:head>
+  <title>{$page.status}: {$page.error.message}</title>
+</svelte:head>
+
+<main>
+  {#if dogImage }
+    <a href="/"><img src={dogImage} class='mx-auto' alt="HTTP status dog!" /></a>
+  {:else}
+    <p class='text-center my-5'>An error occured. Please go <a href="/">HOME</a></p>
+  {/if}
+</main>
