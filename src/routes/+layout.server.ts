07d41396f696ee52734f85c83dc9df6e71737858
Jaarabytes <xavierandole78@gmail.com>
2024-07-07 12:49:58 +0300

    feat: redirect
    Users will be redirected to the sonn page, i don't want them to view my
    work in progress

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,11 @@
+// import type { LayoutServerLoad } from "./$types";
+// import { redirect } from "@sveltejs/kit";
+
+// I don't want users to see the work while we are still in dev, 
+//
+// to get rid of this just comment out this code, all of it
+// export const load : LayoutServerLoad = ({ url }) => {
+//  if ( url.pathname == '/' ) {
+//    throw redirect(307, "/soon")
+//  }
+// }
