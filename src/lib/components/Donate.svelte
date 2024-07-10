8c89edc7c38833a7c41abe94b08a7a583cc1b56d
Jaarabytes <xavierandole78@gmail.com>
2024-07-10 09:16:51 +0300

    style: more descriptive
    Made it more clear that it is Kenyan Shillings instead of KES

--- src/lib/components/Donate.svelte
+++ src/lib/components/Donate.svelte
@@ -32,7 +32,7 @@
 <main>
   <h1 class='font-extrabold text-center text-3xl'>Be blessed!!!</h1>
   <form class="text-center" >
-    <label for='amount' class='text-center'>Amount (in KES)</label><br /> <br />
+    <label for='amount' class='text-center'>Amount (in Kenyan Shillings)</label><br /> <br />
     <input bind:value={amount} class='bg-slate-200 border-2 border-black text-center rounded-lg text-black py-2 px-3' required /><br /><br />
     <button class="bg-green-900 px-3 py-2 rounded-lg text-white hover:bg-green-700" type='button' on:click={payWithPaystack}>Donate</button>
   </form>
