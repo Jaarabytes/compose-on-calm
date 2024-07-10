63c17f3481c9c817ed4d4665c895f840f3132728
Jaarabytes <xavierandole78@gmail.com>
2024-07-10 12:44:16 +0300

    test
    Testing for the screen capture api

---
+++ src/routes/edit/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import "../../App.css"
+  import Input from "$lib/components/Input.svelte";
+  import Recorder from "$lib/components/Recorder.svelte";
+</script>
+<div class='sm:px-[20%] px-3 text-center'>
+  <h1 class="text-3xl font-extrabold my-5">Welcome!</h1>
+  <Recorder />
+  <Input />
+</div>
