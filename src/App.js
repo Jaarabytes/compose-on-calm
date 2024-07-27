8978e9ac4013de61c942ea382af92a54430d5821
jaarabytes <xavierandole78@gmail.com>
2024-07-27 18:06:05 +0300

    turns out it works fine. First demo out. Now for more

--- src/App.js
+++ src/App.js
@@ -38,29 +38,29 @@ function App() {
     const inputFile = `Input.${audio.name.substr(audio.name.lastIndexOf("."))}`
     const outputFile = `Output.${audio.name.substr(audio.name.lastIndexOf("."))}`
 
-
+    console.log(`Conversion started!`)
     const arrayBuffer = await audio.arrayBuffer();
 
     await ffmpeg.writeFile(inputFile, new Uint8Array(arrayBuffer));
     await ffmpeg.exec(["-i", inputFile, "-filter:a", `volume=0.25`, outputFile]);
-    const data = await ffmpeg.readFile('output.mp3')
+    const data = await ffmpeg.readFile(outputFile)
     const url = URL.createObjectURL(new Blob([data.buffer], {type: "audio/mp3"}))
+    console.log(`Conversion complete`)
     setResult(url)
     }
     catch ( err ) {
-      console.error(`Error occured during volume reduction: ${err}`);
+      console.log(`Error occured during volume reduction: ${err}`);
       messageRef.current.innerHTML = `Error: ${err.message}`;
     }
 
   }
   return loaded ? (
     <div className="App">
-    {audio && <audio src={URL.createObjectURL(audio)} />}
-      <input type='file' onChange={(e) => setAudio(e.target.files?.item(0))} controls /><br/>
+      <input type='file' onChange={(e) => setAudio(e.target.files?.item(0))} /><br/>
       <button onClick={changeVolume}>Start</button>
       <p>Press Ctrl + shift + I, to check the logs</p>
     <h3>Logs: </h3>
-    {result && <audio src={changeVolume}/>}
+    {result && <audio src={result} controls />}
       <p ref={messageRef}></p>
     </div>
   ) : (<button onClick={load}>Load resources? (31 MB)</button>);
